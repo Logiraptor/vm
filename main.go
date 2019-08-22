67e2a7a2915d899e9c9479e37a78c7423cee7798
patrickoyarzun <poyarzun@pivotal.io>
2019-08-22 10:23:16 -0700

    Use the basic_block dsl in main

--- main.go
+++ main.go
@@ -13,56 +13,41 @@ func main() {
 		basic_blocks.SetWordInstruction{Destination: core.BRegister, Operand1: 1},
 	}
 	bb2 := basic_blocks.NewBasicBlock("loop")
-	bb2.Instructions = []basic_blocks.Instruction{
-		// loop:
-		// print a
-		basic_blocks.PrintInstruction{Operand1: core.ARegister},
+	loopBuilder := basic_blocks.InstructionBuilder{}
+	// loop:
+	// print a
+	loopBuilder.Print(core.ARegister)
 
-		// add a a b
-		basic_blocks.ArithmeticInstruction{
-			OpCode:      core.AddWord,
-			Destination: core.ARegister,
-			Operand1:    core.ARegister,
-			Operand2:    core.BRegister,
-		},
+	// add a a b
+	loopBuilder.Arithmetic(core.AddWord, core.ARegister, core.ARegister, core.BRegister)
 
-		// swp a b
-		//   set c 0
-		//   set e 0
-		//   store e b
-		//   add b a c
-		//   load e a
-		basic_blocks.SetWordInstruction{Destination: core.CRegister, Operand1: 0},
-		basic_blocks.SetWordInstruction{Destination: core.ERegister, Operand1: 0},
-		basic_blocks.StoreWordInstruction{Address: core.ERegister, Operand1: core.BRegister},
-		basic_blocks.ArithmeticInstruction{
-			OpCode:      core.AddWord,
-			Destination: core.BRegister,
-			Operand1:    core.ARegister,
-			Operand2:    core.CRegister,
-		},
-		basic_blocks.LoadWordInstruction{Destination: core.ARegister, Address: core.ERegister},
+	// swp a b
+	//   set c 0
+	//   set e 0
+	//   store e b
+	//   add b a c
+	//   load e a
+	loopBuilder.SetWord(core.CRegister, 0)
+	loopBuilder.SetWord(core.ERegister, 0)
+	loopBuilder.StoreWord(core.ERegister, core.BRegister)
+	loopBuilder.Arithmetic(core.AddWord, core.BRegister, core.ARegister, core.CRegister)
+	loopBuilder.LoadWord(core.ARegister, core.ERegister)
 
-		// cmp c a 100
-		//   set d 100
-		//   cmp c a d
-		basic_blocks.SetWordInstruction{Destination: core.DRegister, Operand1: 100},
-		basic_blocks.ArithmeticInstruction{
-			OpCode:      core.Compare,
-			Destination: core.CRegister,
-			Operand1:    core.ARegister,
-			Operand2:    core.DRegister,
-		},
+	// cmp c a 100
+	//   set d 100
+	//   cmp c a d
+	loopBuilder.SetWord(core.DRegister, 100)
+	loopBuilder.Arithmetic(core.Compare, core.CRegister, core.ARegister, core.DRegister)
 
-		// beq c -1 loop
-		//   set d -1
-		//   beq c d loop
-		basic_blocks.SetWordInstruction{Destination: core.DRegister, Operand1: -1},
-		basic_blocks.BranchEqualInstruction{Operand1: core.CRegister, Operand2: core.DRegister, BranchTarget: bb2},
+	// beq c -1 loop
+	//   set d -1
+	//   beq c d loop
+	loopBuilder.SetWord(core.DRegister, -1)
+	loopBuilder.BranchEqual(core.CRegister, core.DRegister, bb2)
 
-		// exit 0
-		basic_blocks.ExitInstruction{Operand1: 0},
-	}
+	// exit 0
+	loopBuilder.Exit(0)
+	bb2.Instructions = loopBuilder.Instructions
 	machine := core.CreateVM(0, 8)
 	machine.Instructions = codegen.CodeGen(basic_blocks.ResolveLabels([]*basic_blocks.BasicBlock{
 		bb1, bb2,
